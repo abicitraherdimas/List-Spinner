a3fa43bde65ab5c4698de70af2c75f61cc5968b6
abicitraherdimas <79123967+abicitraherdimas@users.noreply.github.com>
2021-02-16 11:51:06 +0700

    Add files via upload

---
+++ Exam_no2.py
@@ -0,0 +1,36 @@
+'''
+Soal 2 - List Spinner (30 Poin)
+
+Buatlah sebuah return function dengan 1 argumen yang dapat 
+memutar list angka (Putaran 1X counter-clockwise) seperti keterangan di bawah ini .
+
+List awal
+[[1, 2, 3, 4],
+[5, 6, 7, 8],
+[9, 10, 11, 12],
+[13, 14, 15, 16]]
+
+List akhir
+[[4, 8, 12, 16]
+[3, 7, 11, 15],
+[2, 6, 10, 14],
+[1, 5, 9, 13]]
+'''
+
+# list_awal = [[1, 2, 3, 4],
+# [5, 6, 7, 8],
+# [9, 10, 11, 12],
+# [13, 14, 15, 16]]
+
+# for i in list_awal:
+#     if i == [1, 2, 3, 4]:
+#         print("[4, 8 , 12, 16]")
+#         continue
+#     elif i == [5, 6, 7, 8]:
+#         print("[3, 7, 11, 15]")
+#         continue
+#     elif i == [9, 10, 11, 12]:
+#         print("[2, 6, 10, 14]")
+#         continue
+#     else:
+#         print("[1, 5, 9, 13]")
